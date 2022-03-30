00136641e07fba0c6fdcdc55b63f7af0828bbdd9
Michael Koulouvaris <mikekoul@gmail.com>
2022-03-29 20:34:35 -0600

    Update Annotation

--- variables.rb
+++ variables.rb
@@ -1,11 +1,12 @@
+# define variable computer
 computer = "Dell studio"
 phone = "Andriod 15"
 color = "Green"
-
+# define variable guitars as 2
 guitars = 2
 synths = 3
 drums = 1
-
+# define amount of crypto in wallet
 eth = 1.2
 bitcoin = 3.7
 loops = 100
