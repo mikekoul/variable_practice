a92f1e4eadc0a954fafc5b3f8ab8abf757762800
Michael Koulouvaris <mikekoul@gmail.com>
2022-03-29 20:32:45 -0600

    Add Boolean values and Update string variables

--- variables.rb
+++ variables.rb
@@ -1,6 +1,6 @@
-computer = "Macbook Pro"
-phone = "Iphone 12 mini"
-color = "Black"
+computer = "Dell studio"
+phone = "Andriod 15"
+color = "Green"
 
 guitars = 2
 synths = 3
@@ -9,3 +9,7 @@ drums = 1
 eth = 1.2
 bitcoin = 3.7
 loops = 100
+
+hungry = true
+sad = false
+fun = true
